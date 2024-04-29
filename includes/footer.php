a00a8ea53f7d36a63996f1582a3f9c430dfd6e2f
Kai Luis Díaz <luislulo32d@gmail.com>
2024-04-29 13:11:15 -0400

    first step
    Creación de header y footer, así cómo la primera página modificada con include

---
+++ includes/footer.php
@@ -0,0 +1,262 @@
+<footer class="g-bg-bluegray-lineargradient g-color-white-opacity-0_8 g-bg-img-hero g-pt-60">
+        <div class="container g-mb-60">
+          <div class="row">
+            <!-- Footer Content -->
+            <div class="col-lg-3 col-md-6 g-mb-40 g-mb-0--lg">
+              <div class="u-heading-v2-3--bottom g-brd-white-opacity-0_8 g-mb-20">
+                <h2 class="u-heading-v2__title h6 text-uppercase mb-0">Nuestro servicio</h2>
+              </div>
+              <div class="g-mb-30">
+                <p>Supervisa, valida y mejora la experiencia digital de tus clientes.</p>
+              </div>
+              <address class="mb-0">
+                <div class="g-mb-30">
+                  <h4 class="h6 g-font-weight-600">Dirección</h4>
+                  <p class="mb-0">Hyldegårds Tværsvej 47, 4tv.
+                    <br>2920 Charlottenlund. (Copenhagen)
+                    <br>Dinamarca.
+                  </p>
+                </div>
+                <div class="g-mb-30">
+                  <h4 class="h6 g-font-weight-600">Teléfono</h4>
+                  <p class="g-mb-2">España: (+34) 951.23.93.95</p>
+                  <p class="g-mb-10">Dinamarca: (+45) 52.52.05.70</p>
+                </div>
+                <div class="mb-0">
+                  <h4 class="h6 g-font-weight-600">Email</h4>
+                  <a class="g-color-white-opacity-0_8" href="#!">info@qualitasplaza.com</a>
+                </div>
+              </address>
+            </div>
+            <!-- End Footer Content -->
+
+            <!-- Footer Content -->
+            <div class="col-lg-3 col-md-6 g-mb-40 g-mb-0--lg">
+              <div class="u-heading-v2-3--bottom g-brd-white-opacity-0_8 g-mb-20">
+                <h2 class="u-heading-v2__title h6 text-uppercase mb-0">Secciones</h2>
+              </div>
+              <ul class="list-unstyled">
+                <li class="g-mb-5">
+                  <i class="fa fa-angle-right g-mr-5"></i>
+                  <a class="g-color-white-opacity-0_8" href="servicios.html">Servicios</a>
+                </li>
+                <li class="g-mb-5">
+                  <i class="fa fa-angle-right g-mr-5"></i>
+                  <a class="g-color-white-opacity-0_8" href="planes.html">Planes</a>
+                </li>
+              </ul>
+              <br>
+              <div class="u-heading-v2-3--bottom g-brd-white-opacity-0_8 g-mb-20">
+                <h2 class="u-heading-v2__title h6 text-uppercase mb-0">Guía de compras</h2>
+              </div>
+              <ul class="list-unstyled">
+                <li class="g-mb-5">
+                  <i class="fa fa-angle-right g-mr-5"></i>
+                  <a class="g-color-white-opacity-0_8" href="preguntas.html">Preguntas frecuentes</a>
+                </li>
+                <li class="g-mb-5">
+                  <i class="fa fa-angle-right g-mr-5"></i>
+                  <a class="g-color-white-opacity-0_8" href="politicas.html">Política de privacidad</a>
+                </li>
+                <li class="g-mb-5">
+                  <i class="fa fa-angle-right g-mr-5"></i>
+                  <a class="g-color-white-opacity-0_8" href="terminos.html">Términos y condiciones</a>
+                </li>
+              </ul>
+            </div>
+            <!-- End Footer Content -->
+
+            <!-- Footer Content -->
+            <div class="col-lg-3 col-md-6 g-mb-40 g-mb-0--lg">
+              <div class="u-heading-v2-3--bottom g-brd-white-opacity-0_8 g-mb-20">
+                <h2 class="u-heading-v2__title h6 text-uppercase mb-0">Cadena de valor</h2>
+              </div>
+
+              <ul class="list-unstyled">
+                <li class="g-mb-5">
+                  <i class="fa fa-angle-right g-mr-5"></i>
+                  <a class="g-color-white-opacity-0_8" href="partners.html">Partners</a>
+                </li>
+                <li class="g-mb-5">
+                  <i class="fa fa-angle-right g-mr-5"></i>
+                  <a class="g-color-white-opacity-0_8" href="programadores.html">Programadores</a>
+                </li>
+                <li class="g-mb-5">
+                  <i class="fa fa-angle-right g-mr-5"></i>
+                  <a class="g-color-white-opacity-0_8" href="agencias.html">Agencias</a>
+                </li>
+                <li class="g-mb-5">
+                  <i class="fa fa-angle-right g-mr-5"></i>
+                  <a class="g-color-white-opacity-0_8" href="freelancers.html">Freelancers</a>
+                </li>
+              </ul>
+            </div>
+            <!-- End Footer Content -->
+
+            <!-- Footer Content -->
+            <div class="col-lg-3 col-md-6">
+              <div class="u-heading-v2-3--bottom g-brd-white-opacity-0_8 g-mb-20">
+                <h2 class="u-heading-v2__title h6 text-uppercase mb-0 g-font-weight-600">
+                  Síguenos en nuestras redes
+                </h2>
+              </div>
+              <p>Síguenos y recibe información valiosa para poner en valor tu negocio.</p>
+              <ul class="list-inline mb-0">
+                <li class="list-inline-item g-mr-10">
+                  <a class="u-icon-v3 u-icon-size--xs g-bg-white-opacity-5_1 g-bg-white-opacity-0_2--hover g-color-primary-opacity-0_6 rounded"
+                    href="https://www.facebook.com/qualitasplaza/" target="_blank">
+                    <i class="fa fa-facebook"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item g-mr-10">
+                  <a class="u-icon-v3 u-icon-size--xs g-bg-white-opacity-5_1 g-bg-white-opacity-0_2--hover g-color-primary-opacity-0_6 rounded"
+                    href="https://twitter.com/qualitas_plaza/" target="_blank">
+                    <i class="fa fa-twitter"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item g-mr-10">
+                  <a class="u-icon-v3 u-icon-size--xs g-bg-white-opacity-5_1 g-bg-white-opacity-0_2--hover g-color-primary-opacity-0_6 rounded"
+                    href="https://www.linkedin.com/company/qualitas-plaza/" target="_blank">
+                    <i class="fa fa-linkedin"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <!-- End Footer Content -->
+          </div>
+        </div>
+
+        <!-- Copyright Footer -->
+        <div class="g-bg-black-opacity-0_1 g-color-white-opacity-0_8 text-center g-py-20">
+          <div class="container">
+            <small class="g-font-size-default g-mr-10 g-mb-10 g-mb-0--md">2023 &copy; Todos los derechos reservados.
+            </small>
+          </div>
+        </div>
+        <!-- End Copyright Footer -->
+      </footer>
+      <!-- End Footer -->
+    </div>
+
+    <a class="js-go-to u-go-to-v1" href="#!" data-type="fixed" data-position='{
+     "bottom": 15,
+     "right": 15
+   }' data-offset-top="400" data-compensation="#js-header" data-show-effect="zoomIn">
+      <i class="hs-icon hs-icon-arrow-top"></i>
+    </a>
+  </main>
+
+  <div class="u-outer-spaces-helper"></div>
+
+  <!-- JS Dark Skin -->
+
+  <!-- JS Global Compulsory -->
+  <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/jquery-migrate/jquery-migrate.min.js"></script>
+  <script src="assets/vendor/popper.min.js"></script>
+  <script src="assets/vendor/bootstrap/bootstrap.min.js"></script>
+
+  <!-- JS Implementing Plugins -->
+  <script src="assets/vendor/slick-carousel/slick/slick.js"></script>
+  <script src="assets/vendor/hs-megamenu/src/hs.megamenu.js"></script>
+  <script src="assets/vendor/dzsparallaxer/dzsparallaxer.js"></script>
+  <script src="assets/vendor/dzsparallaxer/dzsscroller/scroller.js"></script>
+  <script src="assets/vendor/dzsparallaxer/advancedscroller/plugin.js"></script>
+  <script src="assets/vendor/fancybox/jquery.fancybox.min.js"></script>
+  <script src="assets/vendor/typedjs/typed.min.js"></script>
+
+  <!-- JS Unify -->
+  <script src="assets/js/hs.core.js"></script>
+  <script src="assets/js/components/hs.carousel.js"></script>
+  <script src="assets/js/components/hs.header.js"></script>
+  <script src="assets/js/helpers/hs.hamburgers.js"></script>
+  <script src="assets/js/components/hs.tabs.js"></script>
+  <script src="assets/js/components/hs.popup.js"></script>
+  <script src="assets/js/components/text-animation/hs.text-slideshow.js"></script>
+  <script src="assets/js/components/hs.go-to.js"></script>
+
+  <!-- JS Customization -->
+  <script src="assets/js/custom.js"></script>
+
+  <!-- Defer script iframe Youtube -->
+
+  <script>
+    function init() {
+      var vidDefer = document.getElementsByTagName('iframe');
+      for (var i = 0; i < vidDefer.length; i++) {
+        if (vidDefer[i].getAttribute('data-src')) {
+          vidDefer[i].setAttribute('src', vidDefer[i].getAttribute('data-src'));
+        }
+      }
+    }
+    window.onload = init;
+  </script>
+
+  <!-- JS Plugins Init. -->
+  <script>
+    $(document).on('ready', function () {
+      // initialization of carousel
+      $.HSCore.components.HSCarousel.init('.js-carousel');
+
+      // initialization of tabs
+      $.HSCore.components.HSTabs.init('[role="tablist"]');
+
+      // initialization of popups
+      $.HSCore.components.HSPopup.init('.js-fancybox');
+
+      // initialization of go to
+      $.HSCore.components.HSGoTo.init('.js-go-to');
+
+      // initialization of text animation (typing)
+      $(".u-text-animation.u-text-animation--typing").typed({
+        strings: [
+          "an awesome template",
+          "perfect template",
+          "just like a boss"
+        ],
+        typeSpeed: 60,
+        loop: true,
+        backDelay: 1500
+      });
+    });
+
+    $(window).on('load', function () {
+      // initialization of header
+      $.HSCore.components.HSHeader.init($('#js-header'));
+      $.HSCore.helpers.HSHamburgers.init('.hamburger');
+
+      // initialization of HSMegaMenu component
+      $('.js-mega-menu').HSMegaMenu({
+        event: 'hover',
+        pageContainer: $('.container'),
+        breakpoint: 991
+      });
+    });
+
+    $(window).on('resize', function () {
+      setTimeout(function () {
+        $.HSCore.components.HSTabs.init('[role="tablist"]');
+      }, 200);
+    });
+  </script>
+  <script>
+    document.getElementById('change-theme-btn').addEventListener('click', function () {
+      let darkThemeEnabled = document.body.classList.toggle('dark-theme');
+      localStorage.setItem('dark-theme-enabled', darkThemeEnabled);
+    });
+
+    if (JSON.parse(localStorage.getItem('dark-theme-enabled'))) {
+      document.body.classList.add('dark-theme');
+    }
+  </script>
+  <script>
+    function loadScript(a) { var b = document.getElementsByTagName("head")[0], c = document.createElement("script"); c.type = "text/javascript", c.src = "https://tracker.metricool.com/resources/be.js", c.onreadystatechange = a, c.onload = a, b.appendChild(c) } loadScript(function () { beTracker.t({ hash: "e440703988521b0782ec63597e16ffca" }) });</script>
+
+</body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T9N7H6" height="0" width="0"
+    style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+</html>
