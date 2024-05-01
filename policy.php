96200fc0e52eb803dd3d4e7ed7543c320626e569
Kai Luis Díaz <luislulo32d@gmail.com>
2024-05-01 07:45:16 -0400

    ultima actualización

---
+++ policy.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Title -->
+  <title>Qualitas Plaza - Policy</title>
+  <link rel="canonical" href="https://qualitasplaza.com/policy.html">
+
+  <!-- Required Meta Tags Always Come First -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="assets/img-temp/favicon.png">
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+  <!-- CSS Global Compulsory -->
+  <link rel="stylesheet" href="assets/vendor/bootstrap/bootstrap.min.css">
+  <!-- CSS Global Icons -->
+  <link rel="stylesheet" href="assets/vendor/icon-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="assets/vendor/icon-line/css/simple-line-icons.css">
+  <link rel="stylesheet" href="assets/vendor/icon-etlinefont/style.css">
+  <link rel="stylesheet" href="assets/vendor/icon-line-pro/style.css">
+  <link rel="stylesheet" href="assets/vendor/icon-hs/style.css">
+  <link rel="stylesheet" href="assets/vendor/dzsparallaxer/dzsparallaxer.css">
+  <link rel="stylesheet" href="assets/vendor/dzsparallaxer/dzsscroller/scroller.css">
+  <link rel="stylesheet" href="assets/vendor/dzsparallaxer/advancedscroller/plugin.css">
+  <link rel="stylesheet" href="assets/vendor/animate.css">
+  <link rel="stylesheet" href="assets/vendor/fancybox/jquery.fancybox.min.css">
+  <link rel="stylesheet" href="assets/vendor/slick-carousel/slick/slick.css">
+  <link rel="stylesheet" href="assets/vendor/typedjs/typed.css">
+  <link rel="stylesheet" href="assets/vendor/hs-megamenu/src/hs.megamenu.css">
+  <link rel="stylesheet" href="assets/vendor/hamburgers/hamburgers.min.css">
+
+  <!-- Fontawesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+
+  <!-- Fontawesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <!-- CSS Unify -->
+  <link rel="stylesheet" href="assets/css/unify-core.css">
+  <link rel="stylesheet" href="assets/css/unify-components.css">
+  <link rel="stylesheet" href="assets/css/unify-globals.css">
+
+  <!-- CSS Customization -->
+  <link rel="stylesheet" href="assets/css/custom.css">
+</head>
+
+<body>
+  <main>
+    <!-- Header -->
+    <?php
+      include_once 'includes/header_en.php'
+    ?>
+    <!-- End Header -->
+  <div class="container g-font-weight-300 mb-3 mb-0" style="padding: 20px;">
+    <br><br>
+    <h1>CONFIDENTIALITY, SAFEGUARD AND TREATMENT OF CUSTOMER INFORMATION</h1>
+    <br>
+    <p>The user personal data of our website will be incorporated into a protected file.</p>
+    <p><a href="index.html">qualitasplaza.com</a> will do everything in their power to keep all the information of its clients in good shelter. It will not undertake disclosing info to third parties, not to sell or make public in any way any information received from its customers will solely be used and exclusively for the purpose of carrying out the transactions agreed with its Clients. According to current legal regulations, any Customer may request to Qualitas Plaza that their data must be modified, corrected or removed from its database.</p>
+    <p>Payment service providers as well as payment systems and related technology service providers to whom data are transmitted to carry out transactions under this contract may be required by the law of the State in which they are operated, or By agreements concluded by it, to provide information on these transactions to the authorities or official bodies of other countries, both within and outside the European Union, in the context of combating the financing of terrorism and serious forms of organized crime, and prevention of money laundering.</p>
+    <p>If you require more information or have questions about our privacy policy, please send them by responding in our <a href="support.html">Contact Form.</a></p>
+    <p>In <a href="index.html">qualitasplaza.com</a> we consider that the privacy of our visitors is very important. This Privacy Policy describes in detail the types of personal information that are collected, protected and used by <a href="index.html">qualitasplaza.com</a></p>
+    <br>
+    <h2>REGISTRATION FILES</h2>
+    <p>Like many other websites, <a href="index.html">qualitasplaza.com</a> makes use of log files. Such files simply keep visitors to the site, which is a standard procedure for hosting companies and hosting services analysis. The information included in the log files includes IP address, browser type, internet service provider, date / time stamp, reference / exit pages and possibly the click count. This information is used to analyze trends, manage the website, track user navigation around the website, and gather demographic information. The IP address and other data are not linked to any information that is personally identifiable, these data may be used by the group of companies belonging to Marketing Standard S.A.R.L.
+</p>
+    <br>
+    <h2>COOKIES AND WEB BEACONS</h2>
+    <p><a href="index.html">qualitasplaza.com</a> uses cookies to store information about visitors' preferences, to record user-specific information about the websites they visit and to personalize the content of our website based on the type of browser or other information that the user Sends by these means.</p>
+    <br>
+    <h2>OUR ADVERTISEMENT ALLIES</h2>
+    <p>Some of our advertising partners may use cookies and web beacons on our site. Our allies are:</p>
+    <ul>
+      <li>Adroll</li>
+      <li>Google Adwords</li>
+      <li>Facebook</li>
+      <li>AgileCRM</li>
+    </ul>
+    <p>While each of these advertising partners have their own Privacy Policy, an updated resource for that information can be found here: <a href="policy.html">Privacy Policies.</a></p>
+    <p>You can consult this list to locate the privacy policies of each of our advertising allies.</p>
+    <p>These servers or networks use in their respective advertisements links that appear in <a href="index.html">qualitasplaza.com</a> and that are sent directly to your browser. These automatically receive their IP address when this occurs. Other technologies (such as cookies, JavaScript or Web Beacons) can also be used by our advertising partners to measure the effectiveness of the campaigns and / or customize the content you see on the website.</p>
+    <p><a href="index.html">qualitasplaza.com</a> does not have access or control over these cookies that are used by our advertising allies.</p>
+    <p>If you wish to deactivate these cookies, you can do so by modifying the personal options of your browser. Further information on the handling of cookies of your browser can be obtained in the Websites of that browser.</p>
+    <p>This privacy policy applies only to our online activity and is valid for visitors to our website and refers to information shared and collected there. This policy is not valid for information obtained by channels other than this website.</p>
+    <br>
+    <h2>CONSENT</h2>
+    <p>By using our website, you agree to the terms of this privacy policy.</p>
+  </div>
+    <br>
+          <div class="shortcode-html">
+            <!-- Footer -->
+            <?php
+              include_once 'includes/footer_en.php'
+            ?>
+            <!-- End Footer -->
+          </div>
+
+    <a class="js-go-to u-go-to-v1" href="#!" data-type="fixed" data-position='{
+     "bottom": 15,
+     "right": 15
+   }' data-offset-top="400" data-compensation="#js-header" data-show-effect="zoomIn">
+      <i class="hs-icon hs-icon-arrow-top"></i>
+    </a>
+  </main>
+
+  <div class="u-outer-spaces-helper"></div>
+
+  <!-- JS Dark Skin -->
+  <script src="assets/js/change-theme.js"></script>
+
+  <!-- JS Global Compulsory -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/jquery-migrate/jquery-migrate.min.js"></script>
+  <script src="assets/vendor/popper.min.js"></script>
+  <script src="assets/vendor/bootstrap/bootstrap.min.js"></script>
+
+  <!-- JS Implementing Plugins -->
+  <script src="assets/vendor/slick-carousel/slick/slick.js"></script>
+  <script src="assets/vendor/hs-megamenu/src/hs.megamenu.js"></script>
+  <script src="assets/vendor/dzsparallaxer/dzsparallaxer.js"></script>
+  <script src="assets/vendor/dzsparallaxer/dzsscroller/scroller.js"></script>
+  <script src="assets/vendor/dzsparallaxer/advancedscroller/plugin.js"></script>
+  <script src="assets/vendor/fancybox/jquery.fancybox.min.js"></script>
+  <script src="assets/vendor/typedjs/typed.min.js"></script>
+
+  <!-- JS Unify -->
+  <script src="assets/js/hs.core.js"></script>
+  <script src="assets/js/components/hs.carousel.js"></script>
+  <script src="assets/js/components/hs.header.js"></script>
+  <script src="assets/js/helpers/hs.hamburgers.js"></script>
+  <script src="assets/js/components/hs.tabs.js"></script>
+  <script src="assets/js/components/hs.popup.js"></script>
+  <script src="assets/js/components/text-animation/hs.text-slideshow.js"></script>
+  <script src="assets/js/components/hs.go-to.js"></script>
+
+  <!-- JS Customization -->
+  <script src="assets/js/custom.js"></script>
+
+  <!-- JS Plugins Init. -->
+  <script>
+    $(document).on('ready', function () {
+        // initialization of carousel
+        $.HSCore.components.HSCarousel.init('.js-carousel');
+
+        // initialization of tabs
+        $.HSCore.components.HSTabs.init('[role="tablist"]');
+
+        // initialization of popups
+        $.HSCore.components.HSPopup.init('.js-fancybox');
+
+        // initialization of go to
+        $.HSCore.components.HSGoTo.init('.js-go-to');
+
+        // initialization of text animation (typing)
+        $(".u-text-animation.u-text-animation--typing").typed({
+          strings: [
+            "an awesome template",
+            "perfect template",
+            "just like a boss"
+          ],
+          typeSpeed: 60,
+          loop: true,
+          backDelay: 1500
+        });
+      });
+
+      $(window).on('load', function () {
+        // initialization of header
+        $.HSCore.components.HSHeader.init($('#js-header'));
+        $.HSCore.helpers.HSHamburgers.init('.hamburger');
+
+        // initialization of HSMegaMenu component
+        $('.js-mega-menu').HSMegaMenu({
+          event: 'hover',
+          pageContainer: $('.container'),
+          breakpoint: 991
+        });
+      });
+
+      $(window).on('resize', function () {
+        setTimeout(function () {
+          $.HSCore.components.HSTabs.init('[role="tablist"]');
+        }, 200);
+      });
+  </script>
+
+</body>
+
+</html>
---
+++ questions.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Title -->
+  <title>Qualitas Plaza - Frecuently Questions</title>
+  <link rel="canonical" href="https://qualitasplaza.com/question.html">
+
+  <!-- Required Meta Tags Always Come First -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="assets/img-temp/favicon.png">
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+  <!-- CSS Global Compulsory -->
+  <link rel="stylesheet" href="assets/vendor/bootstrap/bootstrap.min.css">
+  <!-- CSS Global Icons -->
+  <link rel="stylesheet" href="assets/vendor/icon-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="assets/vendor/icon-line/css/simple-line-icons.css">
+  <link rel="stylesheet" href="assets/vendor/icon-etlinefont/style.css">
+  <link rel="stylesheet" href="assets/vendor/icon-line-pro/style.css">
+  <link rel="stylesheet" href="assets/vendor/icon-hs/style.css">
+  <link rel="stylesheet" href="assets/vendor/dzsparallaxer/dzsparallaxer.css">
+  <link rel="stylesheet" href="assets/vendor/dzsparallaxer/dzsscroller/scroller.css">
+  <link rel="stylesheet" href="assets/vendor/dzsparallaxer/advancedscroller/plugin.css">
+  <link rel="stylesheet" href="assets/vendor/animate.css">
+  <link rel="stylesheet" href="assets/vendor/fancybox/jquery.fancybox.min.css">
+  <link rel="stylesheet" href="assets/vendor/slick-carousel/slick/slick.css">
+  <link rel="stylesheet" href="assets/vendor/typedjs/typed.css">
+  <link rel="stylesheet" href="assets/vendor/hs-megamenu/src/hs.megamenu.css">
+  <link rel="stylesheet" href="assets/vendor/hamburgers/hamburgers.min.css">
+
+  <!-- Fontawesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+
+  <!-- Fontawesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <!-- CSS Unify -->
+  <link rel="stylesheet" href="assets/css/unify-core.css">
+  <link rel="stylesheet" href="assets/css/unify-components.css">
+  <link rel="stylesheet" href="assets/css/unify-globals.css">
+
+  <!-- CSS Customization -->
+  <link rel="stylesheet" href="assets/css/custom.css">
+</head>
+
+<body>
+  <main>
+    <!-- Header -->
+    <?php
+      include_once 'includes/header_en.php'
+    ?>
+    <!-- End Header -->
+  <div class="container">
+    <!-- Accordion -->
+    <section>
+      <div class="container g-py-100">
+        <div class="row justify-content-center">
+          <div class="col-lg-9">
+            <!-- Heading -->
+            <div class="text-center g-mb-60">
+              <h2 class="mb-2">Frequently Asked Questions</h2>
+            </div>
+            <!-- End Heading -->
+
+            <div id="accordion" class="u-accordion u-accordion-color-primary" role="tablist" aria-multiselectable="true">
+              <!-- Card -->
+              <div class="card g-brd-none rounded g-mb-20">
+                <div id="accordion-heading-01" class="u-accordion__header g-pa-0" role="tab">
+                  <h5 class="mb-0">
+                      <a class="collapsed d-flex justify-content-between rounded g-px-30 g-py-20" href="#accordion-body-01" data-toggle="collapse" data-parent="#accordion" aria-expanded="false" aria-controls="accordion-body-01">
+                        HOW TO BUY A PLAN?
+                        <span class="u-accordion__control-icon g-color-primary">
+                          <i class="fa fa-angle-down"></i>
+                          <i class="fa fa-angle-up"></i>
+                        </span>
+                      </a>
+                    </h5>
+                </div>
+                <div id="accordion-body-01" class="collapse" role="tabpanel" aria-labelledby="accordion-heading-01" data-parent="#accordion">
+                  <div class="u-accordion__body g-pa-30">
+                    <p>To buy a plan and enjoy our services please follow these steps:</p>
+                    <p>1. Enter our website, on the menu bar, select Pricing and choose the plan type you are interested in.</p>
+                    <p>2. Once you select an option you will automatically be redirected to the section where the subscription is located (with the description and details) and it will also be added to the shopping cart.</p>
+                    <p>3. If you are not registered, go to the "Register" option and fill in the relevant form. This will include your personal and payment data required to subsequently approve your registration.</p>
+                    <p>4. When you successfully make your payment, you will receive an email with the purchase details (item, subscription number, etc.) and your status will be automatically updated reflecting the type of plan you have purchased.</p>
+                  </div>
+                </div>
+              </div>
+              <!-- End Card -->
+
+              <!-- Card -->
+              <div class="card g-brd-none rounded g-mb-20">
+                <div id="accordion-heading-02" class="u-accordion__header g-pa-0" role="tab">
+                  <h5 class="mb-0">
+                      <a class="collapsed d-flex justify-content-between rounded g-px-30 g-py-20 collapse-padding" href="#accordion-body-02" data-toggle="collapse" data-parent="#accordion" aria-expanded="false" aria-controls="accordion-body-02">
+                        DO WEB AUDITS INCLUDE SOLUTIONS WHERE THEY HIGHLIGHT NEGATIVE RESULTS IN THE EVALUATION?
+                        <span class="u-accordion__control-icon g-color-primary">
+                          <i class="fa fa-angle-down"></i>
+                          <i class="fa fa-angle-up"></i>
+                        </span>
+                      </a>
+                    </h5>
+                </div>
+                <div id="accordion-body-02" class="collapse" role="tabpanel" aria-labelledby="accordion-heading-02" data-parent="#accordion">
+                  <div class="u-accordion__body g-pa-30">
+                    In the event that your website and digital platform do not comply with normal evaluation standards, the portal will issue a report of the faults upon  audit completion.
+                  </div>
+                </div>
+              </div>
+              <!-- End Card -->
+
+              <!-- Card -->
+              <div class="card g-brd-none rounded g-mb-20">
+                <div id="accordion-heading-03" class="u-accordion__header g-pa-0" role="tab">
+                  <h5 class="mb-0">
+                      <a class="collapsed d-flex justify-content-between rounded g-px-30 g-py-20" href="#accordion-body-03" data-toggle="collapse" data-parent="#accordion" aria-expanded="false" aria-controls="accordion-body-03">
+                        PLANS, HOW LONG ARE THEY FOR?
+                        <span class="u-accordion__control-icon g-color-primary">
+                          <i class="fa fa-angle-down"></i>
+                          <i class="fa fa-angle-up"></i>
+                        </span>
+                      </a>
+                    </h5>
+                </div>
+                <div id="accordion-body-03" class="collapse" role="tabpanel" aria-labelledby="accordion-heading-03" data-parent="#accordion">
+                  <div class="u-accordion__body g-pa-30">
+                    It depends on the contained services in the plan, because each plan has periods established for compliance.
+                  </div>
+                </div>
+              </div>
+              <!-- End Card -->
+
+              <!-- Card -->
+              <div class="card g-brd-none rounded g-mb-20">
+                <div id="accordion-heading-04" class="u-accordion__header g-pa-0" role="tab">
+                  <h5 class="mb-0">
+                      <a class="collapsed d-flex justify-content-between rounded g-px-30 g-py-20 collapse-padding" href="#accordion-body-04" data-toggle="collapse" data-parent="#accordion" aria-expanded="false" aria-controls="accordion-body-04">
+                        WHAT IS THE DURATION OF MONITORING SERVICES AND PAYMENT?
+                        <span class="u-accordion__control-icon g-color-primary">
+                          <i class="fa fa-angle-down"></i>
+                          <i class="fa fa-angle-up"></i>
+                        </span>
+                      </a>
+                    </h5>
+                </div>
+                <div id="accordion-body-04" class="collapse" role="tabpanel" aria-labelledby="accordion-heading-04" data-parent="#accordion">
+                  <div class="u-accordion__body g-pa-30">
+                    Monitoring services are monthly as are the payments.
+                  </div>
+                </div>
+              </div>
+              <!-- End Card -->
+
+              <!-- Card -->
+              <div class="card g-brd-none rounded g-mb-20">
+                <div id="accordion-heading-06" class="u-accordion__header g-pa-0" role="tab">
+                  <h5 class="mb-0">
+                      <a class="collapsed d-flex justify-content-between rounded g-px-30 g-py-20 collapse-padding" href="#accordion-body-06" data-toggle="collapse" data-parent="#accordion" aria-expanded="false" aria-controls="accordion-body-06">
+                        HOW LONG IS THE DELIVERY TIME OF SERVICE?
+                        <span class="u-accordion__control-icon g-color-primary">
+                          <i class="fa fa-angle-down"></i>
+                          <i class="fa fa-angle-up"></i>
+                        </span>
+                      </a>
+                    </h5>
+                </div>
+                <div id="accordion-body-06" class="collapse" role="tabpanel" aria-labelledby="accordion-heading-06" data-parent="#accordion">
+                  <div class="u-accordion__body g-pa-30">
+                    <p>Delivery times vary according to the service sent to the customer:</p>
+                    <p>- Web quality technical diagnosis (Audit): 5 days from the plan purchase.</p>
+                    <p>- Web / email failure alert: 1 day after delivery of technical report.</p>
+                    <p>- Best practices of e-Commerce audit: 5 days after delivery of technical report.</p>
+                    <p>- Quality seals: 2 days after approval of the audit reports.</p>
+                  </div>
+                </div>
+              </div>
+              <!-- End Card -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+    <br>
+          <div class="shortcode-html">
+            <!-- Footer -->
+            <?php
+              include_once 'includes/footer_en.php'
+            ?>
+            <!-- End Footer -->
+          </div>
+
+    <a class="js-go-to u-go-to-v1" href="#!" data-type="fixed" data-position='{
+     "bottom": 15,
+     "right": 15
+   }' data-offset-top="400" data-compensation="#js-header" data-show-effect="zoomIn">
+      <i class="hs-icon hs-icon-arrow-top"></i>
+    </a>
+  </main>
+
+  <div class="u-outer-spaces-helper"></div>
+
+  <!-- JS Dark Skin -->
+  <script src="assets/js/change-theme.js"></script>
+
+  <!-- JS Global Compulsory -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/jquery-migrate/jquery-migrate.min.js"></script>
+  <script src="assets/vendor/popper.min.js"></script>
+  <script src="assets/vendor/bootstrap/bootstrap.min.js"></script>
+
+  <!-- JS Implementing Plugins -->
+  <script src="assets/vendor/slick-carousel/slick/slick.js"></script>
+  <script src="assets/vendor/hs-megamenu/src/hs.megamenu.js"></script>
+  <script src="assets/vendor/dzsparallaxer/dzsparallaxer.js"></script>
+  <script src="assets/vendor/dzsparallaxer/dzsscroller/scroller.js"></script>
+  <script src="assets/vendor/dzsparallaxer/advancedscroller/plugin.js"></script>
+  <script src="assets/vendor/fancybox/jquery.fancybox.min.js"></script>
+  <script src="assets/vendor/typedjs/typed.min.js"></script>
+
+  <!-- JS Unify -->
+  <script src="assets/js/hs.core.js"></script>
+  <script src="assets/js/components/hs.carousel.js"></script>
+  <script src="assets/js/components/hs.header.js"></script>
+  <script src="assets/js/helpers/hs.hamburgers.js"></script>
+  <script src="assets/js/components/hs.tabs.js"></script>
+  <script src="assets/js/components/hs.popup.js"></script>
+  <script src="assets/js/components/text-animation/hs.text-slideshow.js"></script>
+  <script src="assets/js/components/hs.go-to.js"></script>
+
+  <!-- JS Customization -->
+  <script src="assets/js/custom.js"></script>
+
+  <!-- JS Plugins Init. -->
+  <script>
+    $(document).on('ready', function () {
+        // initialization of carousel
+        $.HSCore.components.HSCarousel.init('.js-carousel');
+
+        // initialization of tabs
+        $.HSCore.components.HSTabs.init('[role="tablist"]');
+
+        // initialization of popups
+        $.HSCore.components.HSPopup.init('.js-fancybox');
+
+        // initialization of go to
+        $.HSCore.components.HSGoTo.init('.js-go-to');
+
+        // initialization of text animation (typing)
+        $(".u-text-animation.u-text-animation--typing").typed({
+          strings: [
+            "an awesome template",
+            "perfect template",
+            "just like a boss"
+          ],
+          typeSpeed: 60,
+          loop: true,
+          backDelay: 1500
+        });
+      });
+
+      $(window).on('load', function () {
+        // initialization of header
+        $.HSCore.components.HSHeader.init($('#js-header'));
+        $.HSCore.helpers.HSHamburgers.init('.hamburger');
+
+        // initialization of HSMegaMenu component
+        $('.js-mega-menu').HSMegaMenu({
+          event: 'hover',
+          pageContainer: $('.container'),
+          breakpoint: 991
+        });
+      });
+
+      $(window).on('resize', function () {
+        setTimeout(function () {
+          $.HSCore.components.HSTabs.init('[role="tablist"]');
+        }, 200);
+      });
+  </script>
+
+</body>
+
+</html>
